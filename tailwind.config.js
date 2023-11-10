2aa5a56a0d46684bd27ca8fcc04adc6b297e5b92
darshanP712 <darshanpdolphinwebsolutions@gmail.com>
2023-11-10 11:18:59 +0530

    10/11 dawn 12 webpack minified + tailwind add

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './config/*.json',
+    './layout/*.liquid',
+    './assets/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/*.json',
+    './templates/customers/*.liquid'	
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
